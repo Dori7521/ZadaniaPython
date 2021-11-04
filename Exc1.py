80e7029fbf40bb66cfb18e0dddc2fa0ad074e750
Dori7521 <lelitodorota@gmail.com>
2021-11-04 16:08:15 +0100

    kolejne zadanie

---
+++ Exc1.py
@@ -0,0 +1,33 @@
+#cmd
+
+# git init newproject
+# py -m pipenv --python 3.10
+# code .
+
+
+age = 20
+print("Mam {} lat.".format(age))
+
+jezyk = 'Python'
+wersja = 3.8
+
+print("Uczę się języka {} w wersji {}".format(jezyk, wersja))
+
+price = 69.99
+print(f'Towar kosztuje {price}.')
+
+pi = 3.1415926535
+
+print(f'Przybliżenie liczby pi: {pi:.3f}')
+
+
+print('-'*40)
+print('Wersja 3.10')
+print('-'*40)
+
+print('='*40)
+print('autor: bleble')
+print('data: 01-01-2021')
+print('='*40)
+
+print('summer', 'time', 'holiday', sep='#')
---
+++ Exc2.py
@@ -0,0 +1,48 @@
+#1
+kwota = 1000
+r=0.03
+n=5
+kwota2 = kwota*(1+r)**n
+print(f'Wartość końcowa inwestycji wynosi: {kwota2:.2f} PLN')
+
+#2
+a = 3
+b = -4
+c = 1
+delta = b**2 - 4*a*c
+print(f'Delta wynosi: {delta}')
+
+#3
+suma=0
+for i in range(10):
+    an=10+4*(i+1)
+    suma+=an
+
+print(f'Suma pierwszych 10 wyrazów ciągu wynosi: {suma}')
+
+#4
+suma1 =0
+for i in range(6):
+    ag = 8*2**(i)
+    suma1+=ag
+print(f'Suma pierwszych 6 wyrazów ciągu wynosi: {suma1}')
+
+#5
+a=1
+b=5
+c=4
+
+Vietea1 =-b/a
+Vietea2=c/a
+print(f'x1 + x2 = {Vietea1} \nx * x2 = {Vietea2}')
+
+#6
+x1 =2
+x2=-4
+y1=4
+y2=5
+
+srx=(x1+x2)/2
+sry=(y1+y2)/2
+
+print(f'Środek odcinka AB: ({srx},{sry})')
