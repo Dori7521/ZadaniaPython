148f49cf147b357bc8345f2286b9240e4dd908fa
Dori7521 <lelitodorota@gmail.com>
2021-11-05 13:41:13 +0100

    Zadanie z liczeniem pierwiastkow rownania kwadratowego

--- Exc3.py
+++ Exc3.py
@@ -6,7 +6,18 @@ ay = 2
 bx = -1
 by =-1
 
-odl_do_kwadratu = (ax+abs(bx))**2 + (ay+abs(by))**2
-odl = math.sqrt(odl_do_kwadratu)
+odl = math.sqrt((ax+abs(bx))**2 + (ay+abs(by))**2)
 
 print(f'Odległość punktów A i B wynosi: {odl}')
+
+#9
+a=1
+b=5
+c=4
+
+delta = b**2 - 4*a*c              # mozna tez delta_sqrt = (delta)**(1/2)
+
+x1 = (-b - math.sqrt(delta))/2*a  # x1 = (-b - delta_sqrt)/2*a
+x2 =  (-b + math.sqrt(delta))/2*a
+
+print(f'x1 = {x1} \nx2 = {x2}')
