b694a658546d861b98a707e46cea2e1b09b2d9fe
Dori7521 <lelitodorota@gmail.com>
2021-11-05 13:23:34 +0100

    Zadanie dotyczace odleglosci dwoch punktow

--- Exc3.py
+++ Exc3.py
@@ -1,1 +1,12 @@
+import math
 print(f'Dalsze roziwazywanie zadan')
+#8
+ax = 3
+ay = 2
+bx = -1
+by =-1
+
+odl_do_kwadratu = (ax+abs(bx))**2 + (ay+abs(by))**2
+odl = math.sqrt(odl_do_kwadratu)
+
+print(f'Odległość punktów A i B wynosi: {odl}')
