50eb8f36eb64890760d7866ad25588c701934619
Dori7521 <lelitodorota@gmail.com>
2021-11-15 19:34:52 +0100

    Poprawione cwiczenie #6

--- Exc10.py
+++ Exc10.py
@@ -40,7 +40,7 @@ else:
 project_ids = ['02134', '24253']
 project_id = '02135'
 
-if not project_id is project_ids:
+if not project_id in project_ids:
     project_ids.append(project_id)
     
 print(project_ids)
