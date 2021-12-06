5c78cbe4fe4f8b815c6254e4136cbf86eb94e621
Dori7521 <lelitodorota@gmail.com>
2021-12-06 16:01:35 +0100

    cisniecie while

--- Exc13.py
+++ Exc13.py
@@ -1,6 +1,9 @@
 # pętla WHILE
 
 #1
+from os import W_OK
+
+
 hmuch = 0
 number = 2
 prime = []
@@ -9,9 +12,9 @@ while hmuch <10:
     for i in range(2,number):
         if number%i==0:
             break
-        else:
-            prime.append(str(number))
-            hmuch+=1
+    else:
+        prime.append(str(number))
+        hmuch+=1
     number+=1
 
 print(','.join(prime))
@@ -104,3 +107,77 @@ except KeyError:
     users.update({user_id:None})
 
 print(users)
+
+
+#1.12
+prime = []
+number = 2
+hmuch = 0
+
+while hmuch<10:
+    for i in range(2,number):
+        if number%i==0:
+            break
+    else:
+        prime.append(str(number))
+        hmuch+=1
+    number+=1
+
+print(','.join(prime))
+
+#2.12
+n = 1
+pv = 1000
+r = 0.04
+n = 1
+pv = 1000
+r = 0.04
+fv = pv*(1+r)
+
+while fv <2000:
+    fv = fv*(1+r)
+    n+=1
+
+print(f'Wartosc przyszla: {fv:.2f} PLN. Liczba okresow: {n} lat.')
+
+#3.12
+max_iters = 10000 
+iters = 0
+w_0 = -1
+previous_step_size = 1
+learning_rate = 0.01
+precision = 0.000001
+derivative = lambda w: 2 * w - 4
+
+while previous_step_size>precision and iters<max_iters:
+    prev = w_0
+    w_0 = w_0-learning_rate*derivative(prev)
+    previous_step_size = abs(w_0 - prev)
+    iters+=1
+
+print(f'Minimum lokalne w punkcie: {w_0:.2f}.')
+
+
+#4.12
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+target = 7
+start = 0
+end = len(numbers)-1
+flag = None
+
+while start <=end:
+    mid = int((start+end)/2)
+
+    if numbers[mid]==target:
+        flag = True
+        break
+    else:
+        if numbers[mid]<target:
+            start = mid+1
+        else:
+            end = mid - 1
+
+if flag:
+    print('Znaleziono')
+else:
+    print('Nie znaleziono')
