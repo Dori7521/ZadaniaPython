e976583e8e8dcf800a7c6674598e5e283528027f
Dori7521 <lelitodorota@gmail.com>
2022-01-07 18:18:23 +0100

    zadania input

--- Exc14-15.py
+++ Exc14-15.py
@@ -1,78 +1,33 @@
 #Input/Output
 
 #1
-with open('playway.txt', 'r') as file:
-    lines = file.read().splitlines()
+# with open('num.txt', 'w') as file:
+#     for i in range(2,19):
+#         if i % 2 == 0:
+#             print(i, file=file)
 
-maxi = []
+even_num = [i for i in range(2,20) if i%2 ==0]
 
-for idx,line in enumerate(lines):
-    if idx>0:
-        maxi.append(float(line.split(',')[2]))
-
-avg = sum(maxi)/len(maxi)
-print(f'Średnia maksymalnych wartosci z 3 dni: {avg:.2f}')
+with open('num.txt', 'w') as file:
+    for num in even_num:
+        file.write(str(num)+'\n')
 
 #2
-with open('indeksy.txt', 'r') as file:
-    lines = file.read().splitlines()
+import json
 
-wig = []
-results = [line for idx, line in enumerate(lines) if line.startswith('WIG')]
-print(results)
+stocks = {'PLW': ['Playway', 350], 'BBT': ['Boombit', 22]}
 
-# for idx, line in enumerate(lines):
-#     if line.startswith('WIG'):
-#         wig.append(line)
-
-# print(f'Nazwy zaczynające się od WIG: {wig}')
+with open('stocks.json', 'w') as file:
+    #json.dump(stocks,file,indient=4)
+    json_stocks = json.dumps(stocks,indent=4)
+    file.write(json_stocks)
 
 #3
-with open('plw_d.csv', 'r') as file:
-    content = file.read().splitlines()
+headers = ['user_id', 'amount']
+users = [['001', '1400'], ['004', '1300'], ['007', '900']]
 
-# data_dict = dict()
-# list_date = list()
-# list_close = list()
+with open('users.csv','w') as file:
+        file.write(','.join(headers) + '\n')
 
-# for idx, line in enumerate(content):
-#     if idx>0:
-#         list_date.append(line.split(',')[0])
-#         list_close.append(line.split(',')[4])
-
-# data_dict['Data'] = list_date
-# data_dict['Zamknięcie'] = list_close
-
-# print(data_dict)
-
-data = [(line.split(',')[0],line.split(',')[4]) for line in content]
-
-data_dict = {
-    data[0][0] : [data[1:][i][0] for i in range (len(data)-1)],
-    data[0][1] : [float(data[1:][i][1]) for i in range (len(data)-1)]
-}
-
-
-print(data_dict)
-
-#4
-with open('plw_d.csv', 'r') as file:
-    content = file.read().splitlines()
-
-wol = [line.split(',')[-1] for line in content][1:]
-wol = [int(w) for w in wol]
-max_wol = max(wol)
-
-print(f'Max Vol: {max_wol}')
-print('-' * 20)
-
-#5
-with open('plw_d.csv', 'r') as file:
-    content = file.read().splitlines()
-
-data = [(line.split(',')[0], line.split(',')[-1]) for line in content][1:]
-data = [(val[0],int(val[1])) for val in data]
-
-max_vol = max(val[1] for val in data)
-max_date = list(filter(lambda val : val[1]==max_vol, data))[0][0]
-print(f'Data: {max_date}')
+        for user in users:
+            file.write(','.join(user) + '\n')
--- Exc14.py
+++ Exc14.py
@@ -100,3 +100,12 @@ print(f'Max Vol: {max_val}')
 
 
 #5
+with open('plw_d.csv', 'r') as file:
+    content = file.read().splitlines()
+
+data = [(line.split(',')[0], line.split(',')[-1]) for line in content][1:]
+data = [(val[0],int(val[1])) for val in data]
+
+max_vol = max(val[1] for val in data)
+max_date = list(filter(lambda val : val[1]==max_vol, data))[0][0]
+print(f'Data: {max_date}')
